0c4e0ae158b90b53749953b5a34e4a0a7e9f002d
David Oluwatimilehin Bamikole <52999385+David1248@users.noreply.github.com>
2024-04-26 10:47:00 +0100

    Update modelling.py

--- modelling/modelling.py
+++ modelling/modelling.py
@@ -4,22 +4,6 @@ from model.decisiontree import DecisionTree
 
 
 def model_predict(data, df, name):
-    '''
-    results = []
-    print("RandomForest")
-    model = RandomForest("RandomForest", data.get_embeddings(), data.get_type())
-    model.train(data)
-    model.predict(data.X_test)
-    model.print_results(data)
-
-    results = []
-    print("DecisionTree")
-    model = DecisionTree("DecisionTree", data.get_embeddings(), data.get_type())
-    model.train(data)
-    model.predict(data.X_test)
-    model.print_results(data)
-    '''
-
     algorithms = [RandomForest, DecisionTree]
     names = ['RandomForest', 'DecisionTree']
     for i in range(len(algorithms)):
